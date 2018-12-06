25ce9d5bbbb84983e0ad602062f91e4d1596df2c
Hellen <hellenlacan1994@gmail.com>
2018-12-05 23:26:59 -0600

    Region Terminales creada

--- Practica1/sol.com.analyzer/Grammar.cs
+++ Practica1/sol.com.analyzer/Grammar.cs
@@ -12,6 +12,37 @@ namespace Practica1.sol.com.analizador
     {
         public Grammar(): base(caseSensitive:true) {
 
+            #region ER
+            RegexBasedTerminal numero = new RegexBasedTerminal("numero", "[0-9]+");
+            #endregion
+
+            #region Terminales
+            var corcheteAb = ToTerm("[");
+            var corcheteCerr = ToTerm("]");
+            var llaveAb = ToTerm("{");
+            var llaveCerr = ToTerm("{");
+            var parentesisAb = ToTerm("(");
+            var parentesisCerr = ToTerm(")");
+            var asignacion = ToTerm("=");
+            var ptoYComa = ToTerm(";");
+            var coma = ToTerm(",");
+            var punto = ToTerm(".");
+            var mas = ToTerm("+");
+            var menos = ToTerm("-");
+            var por = ToTerm("*");
+            var div = ToTerm("/");
+            var menor = ToTerm("<");
+            var mayor = ToTerm(">");
+            var menorQ = ToTerm("<=");
+            var mayorQ = ToTerm(">=");
+            var igual = ToTerm("==");
+            var distinto = ToTerm("!=");
+            var and = ToTerm("&&");
+            var or = ToTerm("||");
+            var negacion = ToTerm("!");
+            var dosPtos = ToTerm(":");
+            #endregion
+
         }
     }
 }
